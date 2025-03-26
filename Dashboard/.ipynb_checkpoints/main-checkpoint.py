6f02c15f11d600d5daa17287054b04891666cf27
annieboltwood <northvannie@gmail.com>
2025-03-25 21:03:52 -0700

    new monthly employee stats page

--- Dashboard/.ipynb_checkpoints/main-checkpoint.py
+++ Dashboard/.ipynb_checkpoints/main-checkpoint.py
@@ -1,12 +1,19 @@
 
-
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import sqlite3
+import streamlit as st
+
+#conn = sqlite3.connect('../timekeeping.db')
 
 st.title("Welcome to the Dashboard!")
 st.write("This is the landing page of the app.")
 
 if st.button("Home"):
     st.switch_page("main.py")
+if st.button("Monthly Employee Stats"):
+    st.switch_page("pages/monthly_employee_stats.py")
 if st.button("Timekeeping Tables"):
     st.switch_page("pages/timekeeping_tables.py")
 
