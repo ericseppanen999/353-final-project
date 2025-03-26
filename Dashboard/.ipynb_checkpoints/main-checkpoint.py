fd615a0c2edf6ab9aaa70ae2121d28f83fb3e1b3
annieboltwood <northvannie@gmail.com>
2025-03-26 02:27:07 -0700

    added new visualiations and modified sizing, colors, and figure placement

--- Dashboard/.ipynb_checkpoints/main-checkpoint.py
+++ Dashboard/.ipynb_checkpoints/main-checkpoint.py
@@ -12,8 +12,8 @@ st.write("This is the landing page of the app.")
 
 if st.button("Home"):
     st.switch_page("main.py")
-if st.button("Monthly Employee Stats"):
-    st.switch_page("pages/monthly_employee_stats.py")
+if st.button("Monthly Stats"):
+    st.switch_page("pages/monthly_stats.py")
 if st.button("Timekeeping Tables"):
     st.switch_page("pages/timekeeping_tables.py")
 
